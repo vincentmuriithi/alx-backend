b50fefde16f89d60b99eba2257b809071a3d03d0
vincent Muriithi <vincentmuriithi06@gmail.com>
2024-05-25 06:29:37 -0700

    3-hypermedia_del_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,14 +4,12 @@ Contains class with methods to create simple pagination from csv data
 """
 import csv
 from typing import List
-index_range = __import__("0-simple_helper_function").index_range
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
     """
-    Server class to paginate a database of popular baby names.
-    """
-
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -42,7 +40,7 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-         Returns a page of the dataset.
+        Returns a page of the dataset.
         Args:
             page (int): The page number.
             page_size (int): The page size.
@@ -71,11 +69,11 @@ class Server:
         total_pages = len(self.dataset()) // page_size + 1
         data = self.get_page(page, page_size)
         info = {
-                "page": page,
-                "page_size": page_size if page_size <= len(data) else len(data),
-                "total_pages": total_pages,
+            "page": page,
+            "page_size": page_size if page_size <= len(data) else len(data),
+            "total_pages": total_pages,
             "data": data,
             "prev_page": page - 1 if page > 1 else None,
             "next_page": page + 1 if page + 1 <= total_pages else None
-            }
+        }
         return info
