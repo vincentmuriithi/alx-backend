e7b2f05ec055365197f9abfec7b8aaa7fc3393ca
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-05-31 11:52:04 -0700

    1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 """
-Creating flask app with babel
+Flask app
 """
-
 from flask import Flask, render_template
 from flask_babel import Babel
 
-class Config:
-    """
-    configuratuons for babel
-    """
 
+class Config(object):
+    """
+    Configuration for Babel
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE ="UTC"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-    
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
-app@route("/")
-def home() -> str:
-    """
-    handles the main page url
-    """
 
-    return render_template("1-index.html")
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
