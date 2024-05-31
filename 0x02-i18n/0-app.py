c6af3f98df0ba3227ce3d5d1b2040bea32a51198
vincentmuriithi <vincentmuriithi06@gmail.com>
2024-05-31 11:03:40 -0700

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,13 +3,15 @@
 Creating a basic flask app
 """
 
+
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/", strict_slashes=False)
+def home() -> str:
     return render_template("0-index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000, host="0.0.0.0", debug=True)
